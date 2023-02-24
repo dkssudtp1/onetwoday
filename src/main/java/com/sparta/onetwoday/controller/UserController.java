ae75ab2a14f8d771ed568006b978a7602a07366c
dkssudtp1 <dkssudtp1@naver.com>
2023-02-24 17:37:09 +0900

    feat : 기초세팅

---
+++ src/main/java/com/sparta/onetwoday/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.sparta.onetwoday.controller;
+
+import com.sparta.onetwoday.dto.LoginRequestDto;
+import com.sparta.onetwoday.dto.SignupRequestDto;
+import com.sparta.onetwoday.entity.Message;
+import com.sparta.onetwoday.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+
+    @ResponseBody
+    @PostMapping("/signup")
+    public ResponseEntity<Message> signup(@Valid SignupRequestDto signupRequestDto) {
+        userService.signup(signupRequestDto);
+
+        Message message = new Message(true, "회원가입 완료", null);
+
+        return ResponseEntity.ok(message);
+    }
+
+    @ResponseBody
+    @PostMapping("/login")
+    public ResponseEntity<Message> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        userService.login(loginRequestDto, response);
+
+        Message message = new Message(true, "로그인 완료", null);
+
+        return ResponseEntity.ok(message);
+    }
+
+    @RequestMapping("/forbidden")
+    public ResponseEntity<Message> getForbidden() {
+        Message message = new Message(true, "로그인을 안했거나 권한이 없습니다.", null);
+
+        return ResponseEntity.badRequest().body(message);
+    }
+}
---
+++ src/main/java/com/sparta/onetwoday/service/UserService.java
@@ -0,0 +1,69 @@
+package com.sparta.onetwoday.service;
+
+import com.sparta.onetwoday.dto.LoginRequestDto;
+import com.sparta.onetwoday.dto.SignupRequestDto;
+import com.sparta.onetwoday.entity.User;
+import com.sparta.onetwoday.entity.UserRoleEnum;
+import com.sparta.onetwoday.jwt.JwtUtil;
+import com.sparta.onetwoday.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    private final PasswordEncoder passwordEncoder;
+    private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    @Transactional
+    public void signup(SignupRequestDto signupRequestDto) {
+        String username = signupRequestDto.getUsername();
+        String password = passwordEncoder.encode(signupRequestDto.getPassword());
+        String email = signupRequestDto.getEmail();
+
+        // 회원 중복 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        // 사용자 ROLE 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (signupRequestDto.isAdmin()) {
+            if (!signupRequestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, password, role);
+        userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        String username = loginRequestDto.getUsername();
+        String password = loginRequestDto.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+        // 비밀번호 확인
+        if(!passwordEncoder.matches(password, user.getPassword())){
+            throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
+    }
+
+}
