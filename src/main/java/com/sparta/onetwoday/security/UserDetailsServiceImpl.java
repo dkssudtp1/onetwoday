b7f16036cb45e15564d61a0c2f41ef70eb01b53e
dkssudtp1 <dkssudtp1@naver.com>
2023-02-24 17:20:09 +0900

    feat : 기초세팅

---
+++ src/main/java/com/sparta/onetwoday/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.sparta.onetwoday.security;
+
+import com.sparta.onetwoday.entity.User;
+import com.sparta.onetwoday.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsImpl loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new UserDetailsImpl(user, user.getUsername());
+    }
+}
