6a2552366a20d470c9b227b3254411491e98876d
dkssudtp1 <dkssudtp1@naver.com>
2023-02-25 09:43:28 +0900

    feat : 깃허브 액션으로 CI/CD

--- src/main/java/com/sparta/onetwoday/controller/UserController.java
+++ src/main/java/com/sparta/onetwoday/controller/UserController.java
@@ -8,14 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
