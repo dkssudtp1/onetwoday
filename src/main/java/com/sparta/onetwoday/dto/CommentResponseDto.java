412bb2cff57beb09d0efbb3979bddb1a6e8a9d52
dkssudtp1 <dkssudtp1@naver.com>
2023-02-27 18:47:58 +0900

    fix : comment에 id값까지 리스폰

--- src/main/java/com/sparta/onetwoday/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/onetwoday/dto/CommentResponseDto.java
@@ -10,12 +10,14 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
-    private String comment;
+    private Long id;
     private String username;
+    private String comment;
     private LocalDateTime createdAt;
 
 
     public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
         this.comment = comment.getComment();
         this.username = comment.getUser().getUsername();
         this.createdAt = comment.getCreatedAt();
